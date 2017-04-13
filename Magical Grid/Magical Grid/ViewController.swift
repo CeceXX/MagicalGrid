c587484a084bee434fd3b82f744dd74a6b2a3e8d
Cesare de Cal <github@cesare.io>
2017-04-13 22:05:02 +0200

    Added Xcode project and implemented first grid

---
+++ Magical Grid/Magical Grid/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Magical Grid
+//
+//  Created by Cesare de Cal on 13/04/17.
+//  Copyright © 2017 Cesare de Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        for j in 0...30 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                self.view.addSubview(cellView)
+            }
+        }
+        
+        self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+    }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
+    }
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
+
