5d3bbc5eab436ce9f5ceaeb88a2e0dc5d10a5c98
Cesare de Cal <github@cesare.io>
2017-04-23 21:20:08 +0200

    Animate Out Zoom Bounce Effect

--- Magical Grid/Magical Grid/ViewController.swift
+++ Magical Grid/Magical Grid/ViewController.swift
@@ -36,6 +36,8 @@ class ViewController: UIViewController {
         self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
+    var selectedCell: UIView?
+    
     func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: view)
         let width = view.frame.width / CGFloat(numViewPerRow)
@@ -45,8 +47,36 @@ class ViewController: UIViewController {
         print(i, j)
         
         let key = "\(i)|\(j)"
-        let cellView = cells[key]
-        cellView?.backgroundColor = .white
+        guard let cellView = cells[key] else { return }
+        
+        if selectedCell != cellView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+                
+            }, completion: nil)
+        }
+        
+        
+        view.bringSubview(toFront: cellView)
+        
+        selectedCell = cellView
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
+            
+            cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+            
+        }, completion: nil)
+        
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                cellView.layer.transform = CATransform3DIdentity
+            }, completion: { (_) in
+                
+            })
+            
+        }
+
     }
     
     fileprivate func randomColor() -> UIColor {
